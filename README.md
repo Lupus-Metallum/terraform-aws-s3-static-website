7fe747aa2cbdedd704f71ea0c291ddb051090e21
Nick Adams <nick@greysky.me>
2022-05-16 17:49:02 -0500

    Updated s3 resource

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # terraform-aws-s3-static-website
+
 Used to publish static website files to an S3 bucket with proper MIME type
 
 ## Example
@@ -6,12 +7,13 @@ Used to publish static website files to an S3 bucket with proper MIME type
 ``` Terraform
 module "s3-static-website" {
   source        = "Lupus-Metallum/s3-static-website/aws"
-  version       = "1.0.6"
+  version       = "1.1.0"
   
-  bucket_id     = aws_s3_bucket.example.id
-  file_path     = "${path.module}/src/my-website-files/" # Where your index.html and TLD web content lives
-  acl           = "public-read"
-  storage_class = "STANDARD"
-  cache_control = "max-age=31536000"
+  bucket_id        = aws_s3_bucket.example.id
+  file_path        = "${path.module}/src/my-website-files/" # Where your index.html and TLD web content lives
+  acl              = "public-read"
+  storage_class    = "STANDARD"
+  cache_control    = "max-age=31536000"
+  content_language = "en-US"
 }
 ```
--- variables.tf
+++ variables.tf
@@ -1,27 +1,33 @@
 variable "bucket_id" {
-  type = string
+  type        = string
   description = "The bucket id to use as the destination for the static website"
 }
 
 variable "file_path" {
-  type = string
+  type        = string
   description = "The base filepath that contains the files to publish, you may need path.module/my/src/dir"
 }
 
 variable "acl" {
-  type = string
-  default = "public-read"
+  type        = string
+  default     = "public-read"
   description = "The object acl to use for this site, defaults to public-read"
 }
 
 variable "storage_class" {
-  type = string
-  default = "STANDARD"
+  type        = string
+  default     = "STANDARD"
   description = "The object storage class to use for this site, defaults to STANDARD"
 }
 
 variable "cache_control" {
-  type = string
-  default = "max-age=1440"
+  type        = string
+  default     = "max-age=1440"
   description = "The header to use for cache control"
+}
+
+variable "content_language" {
+  type        = string
+  default     = "en-US"
+  description = "en-US or en-GB for language header?"
 }
